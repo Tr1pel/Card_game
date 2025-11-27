444f991732d57673c200f51dda9017b9cee6d8dc
Азариан Данил <danialazarian@gmail.com>
2025-11-27 22:00:49 +0300

    Player

---
+++ Code/Domain/Player/IPlayer.cs
@@ -0,0 +1,13 @@
+﻿using Itmo.ObjectOrientedProgramming.Lab3.Domain.Board;
+using Itmo.ObjectOrientedProgramming.Lab3.Domain.Collection;
+
+namespace Itmo.ObjectOrientedProgramming.Lab3.Domain.Player;
+
+public interface IPlayer
+{
+    string Name { get; }
+
+    IPlayerBoard Board { get; }
+
+    ICollection Collection { get; }
+}
---
+++ Code/Domain/Player/Player.cs
@@ -0,0 +1,20 @@
+﻿using Itmo.ObjectOrientedProgramming.Lab3.Domain.Board;
+using Itmo.ObjectOrientedProgramming.Lab3.Domain.Collection;
+
+namespace Itmo.ObjectOrientedProgramming.Lab3.Domain.Player;
+
+public sealed class Player : IPlayer
+{
+    public string Name { get; }
+
+    public IPlayerBoard Board { get; }
+
+    public ICollection Collection { get; }
+
+    public Player(string name, IPlayerBoard board, ICollection collection)
+    {
+        Name = name;
+        Board = board;
+        Collection = collection;
+    }
+}
