7ca0330a243e23be216aeeb11067bd2c5a93c327
Азариан Данил <danialazarian@gmail.com>
2025-12-11 10:05:44 +0300

    catalog

---
+++ Code/Domain/Context/Catalog/CatalogPresets.cs
@@ -0,0 +1,19 @@
+﻿using Itmo.ObjectOrientedProgramming.Lab3.Creatures.ObjectsCreatures;
+using Itmo.ObjectOrientedProgramming.Lab3.Modifiers;
+
+namespace Itmo.ObjectOrientedProgramming.Lab3.Context.Catalog;
+
+public static class CatalogPresets
+{
+    public static ICatalog AddPrototypes(this ICatalog catalog)
+    {
+        catalog.AddToCatalog(new AmuletMaster().WithMagicShield().WithAttackMastery());
+
+        catalog.AddToCatalog(new BattleAnalyst());
+        catalog.AddToCatalog(new MimicChest());
+        catalog.AddToCatalog(new EvilFighter());
+        catalog.AddToCatalog(new ImmortalHorror());
+
+        return catalog;
+    }
+}
